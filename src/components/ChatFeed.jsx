81d82aebcc1dd1a4ea61159be9955b73c5ef2b96
longpham3131 <longpham3131@gmail.com>
2021-08-09 19:59:17 +0700

    update Message Form

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -14,7 +14,7 @@ const ChatFeed = (props) => {
         const keys = Object.keys(messages)
 
         return keys.map((key, index) => {
-            const message = message[key];
+            const message = messages[key];
             const lastMessageKey = index === 0 ? null : keys[index - 1]
             const isMyMessage = userName === message.sender.username;
 
--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,7 +1,37 @@
-const MessageForm = () => {
+import {useState} from 'react'
+import {sendMessage, isTyping} from 'react-chat-engine'
+
+const MessageForm = (props) => {
+    const [value, setValue] = useState('')
+    const {chatId, creds} = props;
     
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const text = value.trim()
+
+        if(text.length > 0) sendMessage(creds, chatId, { text })
+
+        setValue('')
+    }
+
+    const handleChange = (event) => {
+        setValue(event.target.value)
+
+        isTyping(props, chatId)
+
+
+    }
     return (
-        <div>Message Form</div>
+        <form className="message-form" onSubmit={handleSubmit}>
+            <input 
+                className="message-input"
+                placeholder="...Send a message"
+                onChange={handleChange}
+                onSubmit={handleSubmit}
+                value={value}
+            />
+        </form>
     )
 }
 
